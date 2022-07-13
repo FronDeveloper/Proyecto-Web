2cd83801e9db8882b4e073eb95b56568d13d4fe9
FronDeveloper <wilson.valencia20.wv@gmail.com>
2022-07-12 21:06:13 -0500

    Primer Proyecto Web

---
+++ JS/menu.js
@@ -0,0 +1,8 @@
+const hamb = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu-navegacion');
+
+
+hamb.addEventListener('click', () => {
+menu.classList.toggle("activar")
+
+});
